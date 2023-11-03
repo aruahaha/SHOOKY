80fed3f6e72a4e3e55f3f6c0708a55faf4af034f
Dipesh Kewalramani <55015285+aruahaha@users.noreply.github.com>
2023-11-04 03:52:56 +0530

    HomeHead done

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -4,7 +4,7 @@ export default function Home() {
     return (
         <div>
             <div className="home-content">
-                <h1></h1>
+                <h1>Step into style and comfort with SHOOKY. Where every stride tells a story</h1>
             </div>
         </div>
     )
